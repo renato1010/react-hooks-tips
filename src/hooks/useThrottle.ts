957bd9dd2962a1ccf3c57eb637cce4f1a8d21757
renato.perez <renatoperezc@gmail.com>
2022-05-04 19:57:41 -0600

    Add useThrottle custom hook

---
+++ src/hooks/useThrottle.ts
@@ -0,0 +1,23 @@
+import { useEffect, useRef } from "react"
+
+export function useThrottle(fn: () => void, timeout: number): void {
+  const previousRef = useRef<(() => void) | null>(null)
+  const currentRef = useRef<(() => void) | null>(fn)
+  if (previousRef.current !== fn) {
+    currentRef.current = fn
+  }
+
+  useEffect(() => {
+    const handle = setInterval(() => {
+      if (currentRef.current) {
+        currentRef.current()
+        previousRef.current = currentRef.current
+        currentRef.current = null
+      }
+    }, timeout)
+
+    return () => {
+      clearInterval(handle)
+    }
+  }, [timeout])
+}
